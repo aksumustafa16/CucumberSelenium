2f42c4a9697ca36dcd251a099bb498ed322e9aa2
musaksu <musaksu@gmail.com>
2023-02-10 21:41:18 +0100

    Navigation Menu Scenario

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -7,17 +7,17 @@ import io.cucumber.java.en.When;
 public class LoginStepDefs {
 
     @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() {
+    public void the_user_is_on_the_login_page() throws InterruptedException {
         System.out.println("I open browser and I navigated vytrack login page");
     }
 
     @When("the user enter the driver information")
-    public void the_user_enter_the_driver_information() {
+    public void the_user_enter_the_driver_information() throws InterruptedException {
         System.out.println("I put user1 username and UserUser123 password and click login button");
     }
 
     @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() {
+    public void the_user_should_be_able_to_login() throws InterruptedException {
         System.out.println("I verify that title changed to Dashboard");
     }
 
@@ -25,4 +25,10 @@ public class LoginStepDefs {
     public void the_user_enter_sales_manager_information() {
         System.out.println("I put salesmanager85 and UserUser123 and click enter");
     }
+
+    @When("the user enter store manager information")
+    public void the_user_enter_store_manager_information() {
+        System.out.println("I put storemanager101 and UserUser123 and click enter");
+    }
+
 }
---
+++ src/test/java/com/vytrack/step_definitions/NavigationMenuStepDefs.java
@@ -0,0 +1,38 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NavigationMenuStepDefs {
+
+    @When("the user navigates to Fleet, Vehicles")
+    public void the_user_navigates_to_Fleet_Vehicles() {
+        System.out.println("the user navigates to Fleet, Vehicles");
+    }
+
+    @Then("the title should be Vehicles")
+    public void the_title_should_be_Vehicles() {
+        System.out.println("Expected and Actual are matching");
+    }
+
+    @When("the user navigates to Marketing, Campaigns")
+    public void the_user_navigates_to_Marketing_Campaigns() {
+        System.out.println("the user navigates to Marketing, Campaigns");
+    }
+
+    @Then("the title should be Campaigns")
+    public void the_title_should_be_Campaigns() {
+        System.out.println("Expected and Actual are matching");
+    }
+
+    @When("the user navigates to Activities, Calendar Events")
+    public void the_user_navigates_to_Activities_Calendar_Events() {
+        System.out.println("the user navigates to Activities, Calendar Events");
+    }
+
+    @Then("the title should be Calenders")
+    public void the_title_should_be_Calenders() {
+        System.out.println("Expected and Actual are matching");
+    }
+
+}
