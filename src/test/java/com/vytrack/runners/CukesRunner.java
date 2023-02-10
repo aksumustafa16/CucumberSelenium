0f8829abf9d2d9a6614fb9c7f86e97b07696599c
musaksu <musaksu@gmail.com>
2023-02-10 15:10:07 +0100

    Cucumber Start

---
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -0,0 +1,13 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "scr/test/resources/features"
+)
+public class CukesRunner {
+
+}
