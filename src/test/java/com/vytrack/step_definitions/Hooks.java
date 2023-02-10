e29225f77a4855a8a2decd17565e93b8f6acb1e3
musaksu <musaksu@gmail.com>
2023-02-11 00:29:47 +0100

    assert class in junit

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,18 +1,21 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
 
 public class Hooks {
 
     @Before
     public void setUp(){
         System.out.println("\tthis is coming from BEFORE");
+        Driver.get().manage().window().maximize();
     }
 
     @After
     public void tearDown(){
-        System.out.println("\tthis is coming from AFTER\n");
+        Driver.closeDriver();
     }
 
     @Before("@db")
--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -1,19 +1,18 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefs {
 
     @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() throws InterruptedException {
+    public void the_user_is_on_the_login_page() {
         String url = ConfigurationReader.get("url");
         //WebDriver driver = Driver.get();
         //driver.get("url");
@@ -23,17 +22,18 @@ public class LoginStepDefs {
     }
 
     @When("the user enter the driver information")
-    public void the_user_enter_the_driver_information() throws InterruptedException {
+    public void the_user_enter_the_driver_information() {
         String username = ConfigurationReader.get("driver_username");
         String password = ConfigurationReader.get("driver_password");
         LoginPage loginPage = new LoginPage();
-        loginPage.login(username,password);
+        loginPage.login(username, password);
     }
 
     @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() throws InterruptedException {
+    public void the_user_should_be_able_to_login() {
+        BrowserUtils.waitFor(3);
         String actualTitle = Driver.get().getTitle();
-        Assert.assertEquals("Verify title","Dashboard",actualTitle);
+        Assert.assertEquals("Verify title", "Dashboard", actualTitle);
     }
 
     @When("the user enter sales manager information")
@@ -41,7 +41,7 @@ public class LoginStepDefs {
         String username = ConfigurationReader.get("sales_manager_username");
         String password = ConfigurationReader.get("sales_manager_password");
         LoginPage loginPage = new LoginPage();
-        loginPage.login(username,password);
+        loginPage.login(username, password);
     }
 
     @When("the user enter store manager information")
@@ -49,7 +49,19 @@ public class LoginStepDefs {
         String username = ConfigurationReader.get("store_manager_username");
         String password = ConfigurationReader.get("store_manager_password");
         LoginPage loginPage = new LoginPage();
-        loginPage.login(username,password);
+        loginPage.login(username, password);
     }
+
+    @When("the user logs in using {string} and {string}")
+    public void the_user_logs_in_using_and(String username, String password) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username, password);
+    }
+
+    @Then("the title contains {string}")
+    public void the_title_contains(String expectedTitle) {
+        Assert.assertTrue(Driver.get().getTitle().contains(expectedTitle));
+    }
+
 
 }
